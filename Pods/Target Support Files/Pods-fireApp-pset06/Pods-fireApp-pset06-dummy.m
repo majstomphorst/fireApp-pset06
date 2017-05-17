1ffdb3ec28556a4caca654375b54cd7bef29e2e9
Maj <maxim.stomphorst@student.uva.nl>
2017-05-17 21:25:41 +0200

    FIRE everywhere!!! (installed Firebase

---
+++ Pods/Target Support Files/Pods-fireApp-pset06/Pods-fireApp-pset06-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fireApp_pset06 : NSObject
+@end
+@implementation PodsDummy_Pods_fireApp_pset06
+@end
--- fireApp-pset06/AppDelegate.swift
+++ fireApp-pset06/AppDelegate.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?)
+        -> Bool {
+            FIRApp.configure()
+            return true
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
