e89c56e35f2eed4e835b2df3c816c80b93b205c7
majstomphorst <maxim.stomphorst@student.uva.nl>
2017-05-18 21:25:12 +0200

    nice

--- fireApp-pset06/MessageViewController.swift
+++ fireApp-pset06/MessageViewController.swift
@@ -14,6 +14,7 @@ class MessageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         statusHandeler()
+//        self.navigationItem.title = "Name?"
         // Do any additional setup after loading the view.
     }
 
@@ -24,20 +25,23 @@ class MessageViewController: UIViewController {
     
     func statusHandeler() {
         
+        // checks if the user is loged in if not send them to the login / register page
         if FIRAuth.auth()?.currentUser?.uid == nil {
             self.performSegue(withIdentifier: "toLogin", sender: nil)
+        } else {
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            FIRDatabase.database().reference().child("users").child(uid!).observe(.value, with: { (snapshot) in
 
-//            let toLoginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as! LoginViewController
-//            
-//            self.navigationController?.pushViewController(toLoginVC, animated: true)
-//            
-//            
-        
-            
-    }
-        
+                
+                if let user = snapshot.value as? [String: AnyObject] {
+                    self.navigationItem.title = user["username"] as? String
+                }
+                
+            })
+        }
         
         
+        // for bebugging
         if FIRAuth.auth()?.currentUser?.uid != nil {
             print("yes login")
         } else {
