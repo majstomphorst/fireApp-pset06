3e232849e2a07a0005504bc494cbc214cdaced3b
majstomphorst <maxim.stomphorst@student.uva.nl>
2017-05-18 14:33:46 +0200

    pop and unwind

--- fireApp-pset06/LoginViewController.swift
+++ fireApp-pset06/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  fireApp-pset06
 //
 //  Created by Maxim Stomphorst on 17/05/2017.
@@ -9,31 +9,50 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    //MARK: outlets
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        FIRAuth.auth()?.createUser(withEmail: "maxim@s.eu", password: "maxim1", completion: { ( user, error) in
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func returnToLogin(segue: UIStoryboardSegue) {}
+    
+    //Mark: actions
+    @IBAction func loginButton(_ sender: Any) {
+        let email = userNameText.text
+        let password = passwordText.text
+        
+        FIRAuth.auth()?.createUser(withEmail: email!, password: password!, completion: { ( user, error) in
             
             if error != nil {
                 print(error!)
                 return
             }
             // succus auth
-
+            
             guard let uid = user?.uid else {
                 return
             }
-           
-        
+            
+            
             
             // save user
             let ref = FIRDatabase.database().reference(fromURL: "https://fireapp-pset06.firebaseio.com/")
             let userRef = ref.child("users").child(uid)
             
-            let values = ["key": "value"]
-  
+            let values = ["email" : email!, "password": password!]
+            
             userRef.updateChildValues(values, withCompletionBlock: { (err, ref) in
                 
                 if err != nil {
@@ -45,15 +64,6 @@ class ViewController: UIViewController {
             
         })
         
-        
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
---
+++ fireApp-pset06/RegisterViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RegisterViewController.swift
+//  fireApp-pset06
+//
+//  Created by Maxim Stomphorst on 18/05/2017.
+//  Copyright © 2017 Maxim Stomphorst. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+//    @IBAction func backButtonPress(_ sender: Any) {
+//
+//    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
