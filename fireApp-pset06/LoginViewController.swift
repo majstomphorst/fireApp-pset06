786dea2bff3c090d23c55bfa026400d032d0860a
majstomphorst <maxim.stomphorst@student.uva.nl>
2017-05-18 19:43:02 +0200

    register and login working!

--- fireApp-pset06/LoginViewController.swift
+++ fireApp-pset06/LoginViewController.swift
@@ -12,11 +12,27 @@ import Firebase
 class LoginViewController: UIViewController {
 
     //MARK: outlets
-    @IBOutlet weak var userNameText: UITextField!
-    @IBOutlet weak var passwordText: UITextField!
+
+    @IBOutlet weak var emailLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do {
+            try FIRAuth.auth()?.signOut()
+        } catch {
+            print("logout faild")
+        }
+        
+        // if user is not loged in
+        if FIRAuth.auth()?.currentUser?.uid == nil {
+            print("no user logedin")
+        }
+        
+        if FIRAuth.auth()?.currentUser?.uid != nil {
+            print("user logedin!!!!!!!")
+        }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -26,10 +42,39 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // this function privodes a link to RegisterVC to go back with a slide out animation
     @IBAction func returnToLogin(segue: UIStoryboardSegue) {}
     
     //Mark: actions
     @IBAction func loginButton(_ sender: Any) {
+        let email: String = emailLabel.text!
+        let password: String = passwordLabel.text!
+        print(email)
+        print(password)
+        
+        FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+            if error != nil {
+                print(error)
+            }
+            
+            guard let uid = user?.uid else {
+                return
+            }
+            print(uid)
+            
+
+            if FIRAuth.auth()?.currentUser?.uid != nil {
+                print("user logedin!!!!!!!")
+            } else {
+                print("no user logedin")
+            }
+        }
+        
+        emailLabel.text = ""
+        passwordLabel.text = ""
+        
+        
+        
         
     }
 
