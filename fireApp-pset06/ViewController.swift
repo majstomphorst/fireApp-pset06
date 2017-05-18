0f5de00360e9c6679a4d906e56a20226cff3125f
Maj <maxim.stomphorst@student.uva.nl>
2017-05-18 08:48:13 +0200

    user register

--- fireApp-pset06/ViewController.swift
+++ fireApp-pset06/ViewController.swift
@@ -14,15 +14,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        FIRAuth.auth()?.createUser(withEmail: "maxim@s.eu", password: "maxim1", completion: { ( FIRUser, error) in
+        FIRAuth.auth()?.createUser(withEmail: "maxim@s.eu", password: "maxim1", completion: { ( user, error) in
             
             if error != nil {
                 print(error!)
                 return
             }
             // succus auth
+
+            guard let uid = user?.uid else {
+                return
+            }
+           
+        
+            
+            // save user
+            let ref = FIRDatabase.database().reference(fromURL: "https://fireapp-pset06.firebaseio.com/")
+            let userRef = ref.child("users").child(uid)
+            
+            let values = ["key": "value"]
+  
+            userRef.updateChildValues(values, withCompletionBlock: { (err, ref) in
+                
+                if err != nil {
+                    print(err!)
+                    return
+                }
+                // succes
+            })
             
         })
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
